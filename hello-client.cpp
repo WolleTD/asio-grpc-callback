566dd936a5f9adabaf0a47cbcde07b2515e48c98
Eicke Herbertz <wolletd@posteo.de>
2022-10-23 16:01:43 +0200

    add coroutines

--- hello-client.cpp
+++ hello-client.cpp
@@ -1,18 +1,23 @@
 #include "hello.grpc.pb.h"
 #include <asio/bind_executor.hpp>
+#include <asio/co_spawn.hpp>
 #include <asio/experimental/concurrent_channel.hpp>
 #include <asio/io_context.hpp>
 #include <asio/post.hpp>
 #include <asio/steady_timer.hpp>
 #include <asio/thread_pool.hpp>
+#include <asio/use_awaitable.hpp>
 #include <asio/use_future.hpp>
 #include <fmt/format.h>
-#include <future>
 #include <grpcpp/grpcpp.h>
 #include <sstream>
 #include <thread>
 #include <utility>
 
+#ifdef __cpp_impl_coroutine
+using asio::awaitable;
+using asio::use_awaitable;
+#endif
 using fmt::format;
 using fmt::print;
 using grpc::Channel;
@@ -85,16 +90,18 @@ public:
             auto reply = std::make_shared<Reply>();
             auto ctx = std::make_shared<ClientContext>();
 
-            stub_->async()->greet(
-                    ctx.get(), &request, reply.get(),
-                    [reply, ctx, work,
-                     handler = std::forward<decltype(handler)>(handler)](const grpc::Status &status) mutable {
-                        print("gRPC handler is running in thread {}\n", current_thread_id());
-                        auto ex = asio::get_associated_executor(handler, work.get_executor());
-                        asio::post(ex, [status, reply, handler = std::forward<decltype(handler)>(handler)]() mutable {
-                            handler(to_exception_ptr(status), *reply);
-                        });
-                    });
+            using hT = decltype(handler);
+            using dhT = std::decay_t<hT>;
+            auto sp_handler = std::make_shared<dhT>(std::forward<hT>(handler));
+
+            stub_->async()->greet(ctx.get(), &request, reply.get(),
+                                  [reply, ctx, work, sp_handler](const grpc::Status &status) mutable {
+                                      print("gRPC handler is running in thread {}\n", current_thread_id());
+                                      auto ex = asio::get_associated_executor(*sp_handler, work.get_executor());
+                                      asio::post(ex, [status, reply, sp_handler]() {
+                                          (*sp_handler)(to_exception_ptr(status), *reply);
+                                      });
+                                  });
         };
 
         return asio::async_initiate<CompletionToken, void(std::exception_ptr, Reply)>(initiation, token, request);
@@ -172,7 +179,85 @@ StreamRequest makeStreamRequest(const std::string &name, int32_t delay_ms, int32
     return request;
 }
 
-void run(const std::string &addr, const std::string &name) {
+#ifdef __cpp_impl_coroutine
+void run_cpp20(const std::string &addr, const std::string &name) {
+    asio::io_context ctx;
+    asio::thread_pool tp{1};
+    asio::steady_timer timer(ctx);
+
+    auto ctx_tid = current_thread_id();
+    print("Main thread id is: {}\n", current_thread_id());
+    auto p = std::promise<std::string>();
+    auto f = p.get_future();
+    asio::post(tp, [&p]() {
+        auto tid = current_thread_id();
+        p.set_value(tid);
+        print("Thread pool thread is: {}\n", tid);
+    });
+    auto tp_tid = f.get();
+    print("Starting off with a synchronous request...\n");
+    auto client = HelloClient(grpc::CreateChannel(addr, grpc::InsecureChannelCredentials()));
+    auto reply = client.greet(makeRequest(name, 0));
+    print("Received sync response: {}\n", reply.greeting());
+
+    print("That worked, now let's throw some async in there!\n");
+    auto a_client = AsyncHelloClient(ctx, grpc::CreateChannel(addr, grpc::InsecureChannelCredentials()));
+
+    auto error_handler = [](const std::exception_ptr &eptr) {
+        try {
+            if (eptr) { std::rethrow_exception(eptr); }
+        } catch (std::exception &e) { print(stderr, "Coroutine ended with exception: {}\n", e.what()); }
+    };
+
+    auto coro1 = [&]() -> awaitable<void> {
+        auto request = makeRequest(name, 1000);
+        auto reply = co_await a_client.greet(request, use_awaitable);
+        auto tid = current_thread_id();
+        print("Received async response 1 in thread {} ({}): {}\n", tid, tid == tp_tid, reply.greeting());
+    };
+
+    auto coro2 = [&]() -> awaitable<void> {
+        auto request = makeRequest(name, 2000);
+        auto reply = co_await a_client.greet(request, use_awaitable);
+        auto tid = current_thread_id();
+        print("Received async response 2 in thread {} ({}): {}\n", tid, tid == ctx_tid, reply.greeting());
+        print("Restarting timer!\n");
+
+        // This will run last!
+        timer.expires_after(std::chrono::milliseconds(1000));
+        co_await timer.async_wait(use_awaitable);
+        print("Timer started in response 2 expired. Reply was: {}\n", reply.greeting());
+    };
+
+    auto coro3 = [&]() -> awaitable<void> {
+        timer.expires_after(std::chrono::milliseconds(500));
+        co_await timer.async_wait(use_awaitable);
+        auto coro3_1 = [&]() -> awaitable<void> {
+            auto request = makeRequest(name, 1000);
+            auto reply = co_await a_client.greet(request, use_awaitable);
+            auto tid = current_thread_id();
+            print("Received async response 3 in thread {} ({}): {}\n", tid, tid == tp_tid, reply.greeting());
+        };
+        co_spawn(tp, coro3_1, error_handler);
+        auto stream = a_client.greet_stream(makeStreamRequest(name, 100, 10));
+        int i = 1;
+        for (;;) {
+            auto sreply = co_await stream->read_next(use_awaitable);
+            auto tid = current_thread_id();
+            print("Received stream response {} ({}) in thread {} ({}): {}\n", sreply.count(), i++, tid, tid == ctx_tid,
+                  sreply.greeting());
+        }
+    };
+
+    co_spawn(tp, coro1, error_handler);
+    co_spawn(ctx, coro2, error_handler);
+    co_spawn(ctx, coro3, error_handler);
+
+    ctx.run();
+}
+#endif
+
+void run_cpp17(const std::string &addr, const std::string &name) {
     asio::io_context ctx;
     asio::thread_pool tp{1};
     asio::steady_timer timer(ctx);
@@ -254,5 +339,8 @@ int main(int argc, const char *argv[]) {
     auto addr = std::string(argv[1]);
     auto name = std::string(argv[2]);
 
-    run(addr, name);
+    run_cpp17(addr, name);
+#ifdef __cpp_impl_coroutine
+    run_cpp20(addr, name);
+#endif
 }
